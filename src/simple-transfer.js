9a311815038fd93f03c844291a53b2d9b8db3bfd
Mai Vu <mvanvu@gmail.com>
2020-06-04 14:04:09 +0700

    Fix multiple click

--- src/simple-transfer.js
+++ src/simple-transfer.js
@@ -16,6 +16,7 @@ function SimpleTransfer(options) {
     }
 
     var
+        body = document.body,
         offset = function (element) {
             if (!element.getClientRects().length) {
                 return {top: 0, left: 0};
@@ -30,60 +31,76 @@ function SimpleTransfer(options) {
             };
         },
         getTransitionEndEventName = function () {
-            var
-                transitions = {
-                    'transition': 'transitionend',
-                    'OTransition': 'oTransitionEnd',
-                    'MozTransition': 'transitionend',
-                    'WebkitTransition': 'webkitTransitionEnd',
-                },
-                bodyStyle = document.body.style;
+            var transitions = {
+                'transition': 'transitionend',
+                'OTransition': 'oTransitionEnd',
+                'MozTransition': 'transitionend',
+                'WebkitTransition': 'webkitTransitionEnd',
+            };
             for (var transition in transitions) {
-                if (typeof bodyStyle[transition] !== 'undefined') {
+                if (typeof from.style[transition] !== 'undefined') {
                     return transitions[transition];
                 }
             }
+
+            return '';
+        },
+        createTransfer = function () {
+            var transfer = document.createElement('div');
+            transfer.setAttribute('class', options.transferClass || 'temp-transfer-element');
+            transfer.style.transition = options.transition || '1.8s all ease';
+            transfer.style.position = toFixed ? 'fixed' : 'absolute';
+            transfer.style.top = (startPosition.top - fixTop) + 'px';
+            transfer.style.left = (startPosition.left - fixLeft) + 'px';
+            transfer.style.width = fromRect.width + 'px';
+            transfer.style.height = fromRect.height + 'px';
+
+            if (typeof options.transferCss === 'object') {
+                for (var css in options.transferCss) {
+                    transfer.style[css] = options.transferCss[css];
+                }
+            }
+
+            return transfer;
         },
         endEvent = getTransitionEndEventName(),
         toFixed = to.style.position === 'fixed',
-        body = document.body,
         fixTop = toFixed ? body.scrollTop : 0,
         fixLeft = toFixed ? body.scrollLeft : 0,
         startPosition = offset(from),
         endPosition = offset(to),
         fromRect = from.getBoundingClientRect(),
         toRect = to.getBoundingClientRect(),
-        transfer = document.createElement('div');
-
-    transfer.setAttribute('class', options.transferClass || 'temp-transfer-element');
-    transfer.style.transition = options.transition || '1.8s all ease';
-    transfer.style.position = toFixed ? 'fixed' : 'absolute';
-    transfer.style.top = (startPosition.top - fixTop) + 'px';
-    transfer.style.left = (startPosition.left - fixLeft) + 'px';
-    transfer.style.width = fromRect.width + 'px';
-    transfer.style.width = fromRect.height + 'px';
+        transfer = createTransfer();
     body.appendChild(transfer);
 
-    if (typeof options.transferCss === 'object') {
-        for (var css in options.transferCss) {
-            transfer.style[css] = options.transferCss[css];
-        }
+    if (endEvent.length) {
+
+        var endCallBack = function () {
+            transfer.removeEventListener(endEvent, endCallBack);
+            body.removeChild(transfer);
+
+            if (typeof options.callBack === 'function') {
+                options.callBack.call(window, from, to);
+            }
+        };
+
+        transfer.addEventListener(endEvent, endCallBack);
+    } else {
+        // Fallback
+        setTimeout(function () {
+            body.removeChild(transfer);
+
+            if (typeof options.callBack === 'function') {
+                options.callBack.call(window, from, to);
+            }
+        }, 1500);
     }
 
-    var endCallBack = function () {
-        transfer.removeEventListener(endEvent, endCallBack);
-        body.removeChild(transfer);
-
-        if (typeof options.callBack === 'function') {
-            options.callBack.call(window, from, to);
-        }
-    };
-
-    transfer.addEventListener(endEvent, endCallBack);
     setTimeout(function () {
         transfer.style.top = (endPosition.top - fixTop) + 'px';
         transfer.style.left = (endPosition.left - fixLeft) + 'px';
         transfer.style.width = toRect.width + 'px';
         transfer.style.height = toRect.height + 'px';
-    }, 1);
+    }, 100);
 }
